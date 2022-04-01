076a2f7dd33edbab845304d2879a8bcd34943686
Leo Freitas <leo.jo100@hotmail.com>
2022-04-01 09:44:37 -0300

    Find Movies

---
+++ backend/src/main/java/com/poo/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.poo.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.poo.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	
+}
