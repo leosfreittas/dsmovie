67b5ff9b45ca2d7ff9bf42dc89b549505e4e1736
Leo Freitas <leo.jo100@hotmail.com>
2022-04-04 13:33:16 -0300

    First Request

--- frontend/src/components/MovieStars/index.jsx
+++ frontend/src/components/MovieStars/index.jsx
@@ -6,7 +6,7 @@ import './styles.css';
 function MovieStars () {
 
     return (
-        <div classNmae="dsmovie-stars-container">
+        <div className="dsmovie-stars-container">
             <StarFull />
             <StarFull />
             <StarFull />
