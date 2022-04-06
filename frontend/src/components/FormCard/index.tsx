4751bd456a472b5470475ec56b9335f70bee6df6
Leo Freitas <leo.jo100@hotmail.com>
2022-04-06 14:14:40 -0300

    Corrections

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -47,7 +47,6 @@ function FormCard ( {movieId} : Props) {
             }
 
             axios(config).then(response => {
-                console.log(response.data);
                 navigate("/");
             })
 
